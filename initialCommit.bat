da06189e20102c714d45ab07860e381d8d3360c8
RahulRawat95 <rahulrawat92@yahoo.com>
2018-12-23 14:06:14 +0530

    first commit

--- initialCommit.bat
+++ initialCommit.bat
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git remote add origin https://github.com/RahulRawat95/VinSolExercise.git
-git push -u origin master
+git push -u -f origin master
 pause
